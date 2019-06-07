d9b0d5591bbac13df5b2f0ed53ff41fc82ab57a5
mrkchoi <kennethichoi@gmail.com>
2019-06-07 14:07:48 -0700

    Add user auth according to spec

--- GoalsApp/spec/models/user_spec.rb
+++ GoalsApp/spec/models/user_spec.rb
@@ -13,7 +13,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:user) do
+    FactoryBot.build(:user,
+      username: "Calvin",
+      password: ""
+    )
+  end
+
   # validations
   it { should validate_presence_of(:username) }
   it { should validate_presence_of(:password_digest) }
@@ -24,44 +30,36 @@ RSpec.describe User, type: :model do
 
 
   # class/instance methods
+  it "makes sure that a user has a session token after initialize" do
+    expect(user.session_token).to_not be_nil
+  end
+  it "makes sure that a user has a password digest after given a password" do
+    expect(user.password_digest).to_not be_nil
+  end
+
   describe '::find_by_credentials' do
     it "should find a user by their username & password" do
-
-    end
-  end
-  
-  describe '#ensure_session_token' do
-    it "makes sure that a user has a session token after initialize" do
-
+      user.password = "password"
+      user.save
+      expect(User.find_by_credentials("Calvin", "password")).to eq(user)
     end
   end
   
   describe '#reset_session_token!' do
     it "should reset a user's session token" do
-
+      old_session_token = user.session_token
+      user.reset_session_token!
+      expect(user.session_token).to_not eq(old_session_token)
     end
   end
   
   describe '#is_password?' do
     it "should validate a user's password" do
-
+      user.password = "password"
+      expect(user.is_password?("password")).to be(true)
     end
   end
   
-  describe '#password=' do
-    context 'with invalid params' do
-      it "should not assign a password digest for a user" do
-        let(:user) { User.new(username: 'calvin', password: 'password') }
-        
-      end
-    end
-    
-    context 'with valid params' do
-      it "should assign a password digest for a user" do
-        
-      end
-    end
-  end
 
 
 
